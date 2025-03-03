6d2a9ae4d3a9a98d09628e9bc3594e3ad22be81e
MaartenSlebos <maarten.slebos@hotmail.com>
2025-03-03 13:01:08 +0100

    add some debugging in console

--- script.js
+++ script.js
@@ -1,17 +1,36 @@
 function getComputerChoice() {
     let randomInt = Math.floor(Math.random() * 3); 
     if (randomInt == 0) {
-        return 'rock'
+        let computerChoice = 'rock'
+        return computerChoice;
     } else if (randomInt == 1) {
-        return 'paper'
+        let computerChoice = 'paper'
+        return computerChoice;
     } else {
-        return 'scissors'
+        let computerChoice = 'scissors'
+        return computerChoice;
     }
+    
 }
 
 function getHumanChoice() {
     let humanInput = window.prompt('Choose: Rock, Paper, or Scissors');
-    let formattedInput = humanInput.toLowerCase();
-    return formattedInput
+    let humanChoice = humanInput.toLowerCase();
+    return humanChoice;
 }
 
+function playRound(computerChoice, humanChoice) {
+    console.log('Computer choice: ' + computerChoice)
+    console.log('Human choice: ' + humanChoice)
+    if (humanChoice == 'rock' && computerChoice == 'scissors') {
+        console.log("You win! Rock beats scissors!")
+    } else {
+        console.log('it was a different outcome')
+    }
+}
+
+let humanScore = 0;
+let computerScore = 0;
+
+
+playRound(getComputerChoice(),getHumanChoice())

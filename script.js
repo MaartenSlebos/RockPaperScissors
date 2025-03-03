407b0375ce1abb665a60ba7d5117b91ca5df8cfe
MaartenSlebos <maarten.slebos@hotmail.com>
2025-03-03 12:43:43 +0100

    get computer choice get human choice

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log("hello world")
+function getComputerChoice() {
+    let randomInt = Math.floor(Math.random() * 3); 
+    if (randomInt == 0) {
+        return 'rock'
+    } else if (randomInt == 1) {
+        return 'paper'
+    } else {
+        return 'scissors'
+    }
+}
+
+function getHumanChoice() {
+    let humanInput = window.prompt('Choose: Rock, Paper, or Scissors');
+    let formattedInput = humanInput.toLowerCase();
+    return formattedInput
+}
+
